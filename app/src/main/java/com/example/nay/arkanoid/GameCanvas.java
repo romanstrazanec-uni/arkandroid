12f3ff64b95acdc0dff3bb9ec182cdc73be5ba34
romanstrazanec <roman.strazanec007@gmail.com>
2018-04-20 08:02:44 +0200

    updatethread

---
+++ app/src/main/java/com/example/nay/arkanoid/GameCanvas.java
@@ -0,0 +1,11 @@
+package com.example.nay.arkanoid;
+
+import android.content.Context;
+import android.view.View;
+
+public class GameCanvas extends View {
+
+    public GameCanvas(Context context) {
+        super(context);
+    }
+}
